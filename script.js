5e3946c7edf3d27310806942481c2749ff24eab4
Scott Wright <scott.wright.uk@gmail.com>
2023-02-28 08:54:17 +0000

    feat: add playRound function to determine winner

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-// define array called "choices" with three options as strings.
-const choices = ["Rock", "Paper", "Scissors"];
-// arrow function to randomly select one of the choices from array
+const choices = ["rock", "paper", "scissors"];
+
+// arrow function to randomly select one of the three choices from array.
 const getComputerChoice = () => {
-  // Generate a random integer between 0 and 2
   const randomNumber = Math.floor(Math.random() * 3);
-  // Return the choice at the randomly generated index
   return choices[randomNumber];
 };
+
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  // check to ensure playerSelection is valid by comparing playerChoice with choices array
+  if (!choices.includes(playerSelection)) {
+    return `Invalid choice! Please choose either ${choices.join(", ")}.`;
+  }
+  // compare choices to determine winner of round
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  }
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}!`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}!`;
+  }
+}
