acefd7a92d871ba05cab25df5db4c207cc356654
Scott Wright <scott.wright.uk@gmail.com>
2023-03-01 12:22:06 +0000

    feat: add game functionality which plays 5 rounds.
    This commit adds the game functionality that allows users to play 5 rounds of the game. It also includes error handling for invalid and empty inputs.
    
    The code uses an array to store the possible choices of rock, paper, or scissors. It generates a random computer choice using the Math library. It then prompts the player for their choice, with case insensitivity and error handling for invalid or empty inputs.
    
    The game logic is implemented using a for loop that plays 5 rounds, and a series of if/else statements that determine the winner of each round. The final score is then displayed based on the number of rounds won by the player and computer.

--- script.js
+++ script.js
@@ -1,46 +1,84 @@
 const choices = ["rock", "paper", "scissors"];
 
+// generate random computer choice
 const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 };
 
-// prompt for player choice input and remove case sensitivity
-const playerSelection = prompt(
-  "Enter your choice: rock, paper, or scissors"
-).toLowerCase();
-// catch when player presses cancel`
-if (playerSelection === null) {
-  console.log(
-    "Your game has been cancelled. If you wish to play again, please refresh your browser!"
-  );
-  // catch when player enters an empty string
-} else if (playerSelection === "") {
-  console.log(
-    "You didn't type anything! Please type either rock, paper or scissors."
-  );
-} else {
-  const computerSelection = getComputerChoice();
-
-  function playRound(playerSelection, computerSelection) {
-    // catch when player makes invalid choice
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  // play 5 rounds
+  for (let roundCount = 1; roundCount <= 5; roundCount++) {
+    // prompt for player choice input and remove case sensitivity
+    let playerSelection = prompt(
+      `Enter your choice: rock, paper, or scissors. Round ${roundCount} of 5.`
+    );
+    // catch when player presses cancel
+    if (playerSelection === null) {
+      console.log(
+        "Your game has been cancelled. If you wish to play again, please refresh your browser!"
+      );
+      return;
+      // catch when player enters an empty string
+    } else if (playerSelection === "") {
+      console.log(
+        "You didn't type anything! Refresh the browser then type either rock, paper or scissors."
+      );
+      // do not count as a round
+      roundCount--;
+      continue;
+    }
+    playerSelection = playerSelection.toLowerCase();
+    // get computer's choice
+    const computerSelection = getComputerChoice();
+    // catch if player makes an invalid input
     if (!choices.includes(playerSelection)) {
-      return `Invalid choice! Please choose either ${choices.join(", ")}.`;
+      console.log(
+        `Invalid choice! Please choose either ${choices.join(", ")}.`
+      );
+      // do not count as a round
+      roundCount--;
+      continue;
     }
+    // tie game cases
     if (playerSelection === computerSelection) {
-      return "It's a tie!";
-    }
-    if (
+      console.log(
+        `Round ${roundCount}: It's a tie! Both players chose ${playerSelection} `
+      );
+      // winning cases
+    } else if (
       (playerSelection === "rock" && computerSelection === "scissors") ||
       (playerSelection === "scissors" && computerSelection === "paper") ||
       (playerSelection === "paper" && computerSelection === "rock")
     ) {
-      return `You win! ${playerSelection} beats ${computerSelection}!`;
+      console.log(
+        `Round ${roundCount}: You win! ${playerSelection} beats ${computerSelection}!`
+      );
+      playerScore++;
+      // losing cases
     } else {
-      return `You lose! ${computerSelection} beats ${playerSelection}!`;
+      console.log(
+        `Round ${roundCount}: You lose! ${computerSelection} beats ${playerSelection}!`
+      );
+      computerScore++;
     }
   }
-
-  const roundResult = playRound(playerSelection, computerSelection);
-  console.log(roundResult);
+  // print final result of the game
+  if (playerScore > computerScore) {
+    console.log(
+      `Result: You won the game! Congratulations! The final score was: you ${playerScore} computer: ${computerScore}.`
+    );
+  } else if (playerScore < computerScore) {
+    console.log(
+      `Result: You lost the game! The final score was: you ${playerScore} computer: ${computerScore}.`
+    );
+  } else {
+    console.log(
+      "Result: It's a tie game! Try again to see if you can beat the computer!"
+    );
+  }
 }
+
+game();
